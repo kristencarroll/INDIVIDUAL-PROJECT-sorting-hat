5ce527490ee91f26bc3fcc9089cd4100ecfbcd1c
Kristen Carroll <carroll.kristen@gmail.com>
2022-10-22 13:07:54 -0500

    Initializeed sorting repo with data list in main.js, back-ground color in main.css and boilerplate and button div in index.html.  No functionality yet

---
+++ main.js
@@ -0,0 +1,23 @@
+//Initialize an array of objects that represent first year students
+const firstYears = [
+  {
+    id: 1,
+    name: "Kristen",
+    house: "Hufflepuff"
+  },
+  {
+    id: 2,
+    name: "Jack",
+    house: "Gryffindor",
+  },
+  {
+    id: 3,
+    name: "Alma",
+    house: "Ravenclaw",
+  },
+  {
+    id: 4,
+    name: "Jackson",
+    house: "Slytherin",
+  }
+];
